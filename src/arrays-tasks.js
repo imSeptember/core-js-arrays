a7434693a521f3902d0d72906f0559858d9648da
Vladislav Kalenskiy <108691131+imSeptember@users.noreply.github.com>
2024-01-18 18:19:18 +0200

    Update arrays-tasks.js

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -20,8 +20,8 @@
  *    getIntervalArray(0, 100) => [ 0, 1, 2, ..., 100 ]
  *    getIntervalArray(3, 3) => [ 3 ]
  */
-function getIntervalArray(/* start, end */) {
-  throw new Error('Not implemented');
+function getIntervalArray(start, end) {
+  return Array.from({ length: end - start + 1 }, (_, index) => start + index);
 }
 
 /**
@@ -37,8 +37,14 @@ function getIntervalArray(/* start, end */) {
  *    sumArrays([10, 20, 30], [5, 10, 15]) => [15, 30, 45]
  *    sumArrays([-1, 0, 1], [1, 2, 3, 4]) => [0, 2, 4, 4]
  */
-function sumArrays(/* arr1, arr2 */) {
-  throw new Error('Not implemented');
+function sumArrays(arr1, arr2) {
+  if (arr1.length > arr2.length) {
+    arr2.push(...Array(arr1.length - arr2.length).fill(0));
+  } else if (arr1.length < arr2.length) {
+    arr1.push(...Array(arr2.length - arr1.length).fill(0));
+  }
+
+  return arr1.map((value, index) => value + arr2[index]);
 }
 
 /**
